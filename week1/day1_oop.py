d2be38293bc095dbb833ecf4d2731e241a1c6271
Sakethkatukam <katukamsaketh@gmail.com>
2026-05-11 00:01:39 +0530

    Day 1: OOP complete - Employee, Developer, Manager classes with classmethods and staticmethods

--- week1/day1_oop.py
+++ week1/day1_oop.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class Employee:
     raise_amount = 1.04
     def __init__(self, first, last, salary):
@@ -5,6 +6,17 @@ class Employee:
         self.last = last
         self.salary = salary
     
+    @staticmethod
+    def is_workday(day):
+        if day.weekday()==5 or day.weekday()==6:
+            return False
+        return True
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first,last,salary=emp_str.split("-")
+        return cls(first, last, int(salary))
+
     def full_name(self):
         return "{} {}".format(self.first, self.last)
 
@@ -37,10 +49,15 @@ class Manager(Employee):
     def remove_employee(self,emp):
         self.employees.remove(emp)
     
-emp1 = Employee("John", "Doe", 50000)
-print(emp1.full_name())
-emp1.print_email()
-mgr1=Manager("J","Smith",90000,[emp1])
-print(mgr1.full_name())
-dev1=Developer("Jane","Doe",60000,"Java")
-print(dev1.full_name()+' -> '+dev1.language)
+# emp1 = Employee("John", "Doe", 50000)
+emp_str_1="John-Doe-70000"
+emp1=Employee.from_string(emp_str_1)
+# print(emp1.full_name())
+emp1.apply_raise()
+print(emp1.salary)
+# print(emp1.is_workday(datetime(2026,5,2)))
+# emp1.print_email()
+# mgr1=Manager("J","Smith",90000,[emp1])
+# print(mgr1.full_name())
+# dev1=Developer("Jane","Doe",60000,"Java")
+# print(dev1.full_name()+' -> '+dev1.language)
